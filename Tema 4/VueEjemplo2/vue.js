d945329bb30852b0acfebda66845cefc1154efdf
mmb-96 <mmb_96@hotmail.com>
2019-11-08 10:45:45 +0100

    Corrección del ejemplo vue 2
    Fallo en colocación del div#app

--- Tema 4/VueEjemplo2/vue.js
+++ Tema 4/VueEjemplo2/vue.js
@@ -1,74 +1,66 @@
 const NYTBaseUrl = "https://api.nytimes.com/svc/topstories/v2/";
-const ApiKey = "Dn09FIkfzgp0xvq3Fraeyy0o9shlcI6w"; // replace with NYT API key 
+const ApiKey = "ADKhr7aBvKthHzQ3arc8OR0IJxapWdGH";
 const SECTIONS = "home, arts, automobiles, books, business, fashion, food, health, insider, magazine, movies, national, nyregion, obituaries, opinion, politics, realestate, science, sports, sundayreview, technology, theater, tmagazine, travel, upshot, world";
 
-function buildUrl(url) {
-  return 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/nytimes-api.json';
+function buildUrl (url) {
+  return NYTBaseUrl + url + ".json?api-key=" + ApiKey
 }
 
 Vue.component('news-list', {
   props: ['results'],
-  template: `
+   template: `
     <section>
       <div class="row" v-for="posts in processedPosts">
         <div class="columns large-3 medium-6" v-for="post in posts">
           <div class="card">
-          <div class="card-divider">
-          {{ post.title }}
+            <div class="card-divider">
+              {{ post.title }}
+            </div>
+            <a :href="post.url" target="_blank"><img :src="post.image_url"></a>
+            <div class="card-section">
+              <p>{{ post.abstract }}</p>
+            </div>
           </div>
-          <a :href="post.url" target="_blank"><img :src="post.image_url"></a>
-          <div class="card-section">
-            <p>{{ post.abstract }}</p>
-          </div>
-        </div>
         </div>
       </div>
-  </section>
+    </section>
   `,
   computed: {
     processedPosts() {
       let posts = this.results;
-
+  
       // Add image_url attribute
       posts.map(post => {
         let imgObj = post.multimedia.find(media => media.format === "superJumbo");
         post.image_url = imgObj ? imgObj.url : "http://placehold.it/300x200?text=N/A";
       });
-
+  
       // Put Array into Chunks
-      let i, j, chunkedArray = [],
-        chunk = 4;
-      for (i = 0, j = 0; i < posts.length; i += chunk, j++) {
-        chunkedArray[j] = posts.slice(i, i + chunk);
+      let i, j, chunkedArray = [], chunk = 4;
+      for (i=0, j=0; i < posts.length; i += chunk, j++) {
+        chunkedArray[j] = posts.slice(i,i+chunk);
       }
       return chunkedArray;
     }
   }
 });
-
+  
 const vm = new Vue({
   el: '#app',
   data: {
     results: [],
     sections: SECTIONS.split(', '), // create an array of the sections
     section: 'home', // set default section to 'home'
-    loading: true,
-    title: ''
   },
-  mounted() {
-    this.getPosts('home');
+  mounted () {
+    this.getPosts(this.section);
   },
   methods: {
     getPosts(section) {
-      let url = buildUrl(section);
-      axios.get(url).then((response) => {
-        this.loading = false;
-        this.results = response.data.results;
-        let title = this.section !== 'home' ? "Top stories in '" + this.section + "' today" : "Top stories today";
-        this.title = title + "(" + response.data.num_results + ")";
-      }).catch((error) => {
-        console.log(error);
-      });
+    let url = buildUrl(section);
+    axios.get(url).then((response) => {
+      this.results = response.data.results;
+    }).catch( error => { console.log(error); });
     }
   }
 });
